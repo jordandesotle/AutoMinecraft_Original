ee9efd51ecd8d5cd806776533ac969fbd4bcbbcb
Jordandesotle <47394662+jordandesotle@users.noreply.github.com>
2023-11-03 14:39:40 -0400

    Hello World

--- src/main/java/jordandesotle/autominecraft/AutoMinecraft.java
+++ src/main/java/jordandesotle/autominecraft/AutoMinecraft.java
@@ -7,7 +7,7 @@ public final class AutoMinecraft extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        System.out.println("Hello World");
     }
 
     @Override
