082759068ef2ee80c301cbbe2d977e4a22f8d996
Jordandesotle <47394662+warwickrobotics@users.noreply.github.com>
2023-11-03 14:38:26 -0400

    Initial commit

---
+++ src/main/java/jordandesotle/autominecraft/AutoMinecraft.java
@@ -0,0 +1,17 @@
+package jordandesotle.autominecraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AutoMinecraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
